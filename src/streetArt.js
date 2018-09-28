3ebfc262a9885ebb96479ebc40c9e67ba80be887
Jowita Stachowiak <crazylittlepolka@gmail.com>
2018-09-28 19:09:58 +0200

    Initial commit

---
+++ src/streetArt.js
@@ -0,0 +1,43 @@
+export const streetArt =
+[
+	{
+		id: 0, 
+		title: 'Confused Face', 
+		category: 'graffitti',
+		lat: 52.238692,
+		lng: 20.994728, 
+		imageUrl: 'image/...' 
+	},	
+		{
+		id: 1, 
+		title: 'Owl', 
+		category: 'graffitti',
+		lat: 52.228819,
+		lng: 20.994546, 
+		imageUrl: 'image/...' 
+	},
+	{
+		id: 2, 
+		title: 'Zefir',
+		category: 'graffitti', 
+		lat: 52.228819,
+		lng: 20.994546, 
+		imageUrl: 'image/...' 
+	},
+	{
+		id: 3, 
+		title: 'Kino Relax',
+		category: 'neon', 
+		lat: 52.232962,
+		lng: 21.010927, 
+		imageUrl: 'image/...' 
+	},
+	{
+		id: 4, 
+		title: 'Owl',
+		category: 'sign', 
+		lat: 52.246037,
+		lng: 21.017535, 
+		imageUrl: 'image/...' 
+	}
+]
